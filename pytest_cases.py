8055d5acfc5091df0b7d0fb3560a54a238a30acb
Max <51382654+Max9Mask@users.noreply.github.com>
2023-10-14 21:16:56 +0200

    Update pytest_cases.py

--- pytest_cases.py
+++ pytest_cases.py
@@ -5,7 +5,7 @@ import pytest
 # Connection to DB
 @pytest.fixture(scope="module")
 def db_connection():
-    server = 'localhost:8080'
+    server = 'EPPLKATW004E:8080'
     user = 'test_login'
     password = '123456abcdefABC'
     db_name = 'AdventureWorks2012'
